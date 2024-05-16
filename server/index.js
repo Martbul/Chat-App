eee39092dc230d11cf1dd5f27f93db12cbd2fe0c
Martbul <martbul01@gmail.com>
2024-05-17 00:25:08 +0300

    add register functionality
    server can register users in cloud mongoDB with mongoose(validation included and error handling)

--- server/index.js
+++ server/index.js
@@ -1,12 +1,27 @@
-const express = require("express")
-const cors = require("cors")
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+const userRoute = require('./Routes/userRoute');
 
 const app = express()
-const port = 5000
+require("dotenv").config()
+const port = process.env.PORT || 5000; //this env port is set automatically by your hosting service;
+const uri = process.env.ATLAS_URI;
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
+app.use("/api/users", userRoute);
+
+
 
 app.listen(port,(req,res) =>{
-    console.log(`Server is running on port: ${port}`);
+    console.log(`Server is runing on port: ${port}`)
+});
+
+mongoose.connect(uri,{
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
 })
+.then(()=>console.log('Connected to MongoDB!'))
+.catch((error) =>console.log("MongoDB connection FAILED:", error.message));
