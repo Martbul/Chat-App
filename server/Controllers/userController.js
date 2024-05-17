c25455ff1c662f1c80c07457c60150a2434c0a8f
Martbul <martbul01@gmail.com>
2024-05-17 08:06:35 +0300

    add user APIs
    login, findUsers and findAllUsers are 3 new APIs

--- server/Controllers/userController.js
+++ server/Controllers/userController.js
@@ -3,11 +3,13 @@ const bcrypt = require("bcrypt");
 const validator = require("validator");
 const jwt = require("jsonwebtoken");
 
+
 const createToken = (_id) => {
     const jwtkey = process.env.JWT_SECRET_KEY;
 
     return jwt.sign({ _id }, jwtkey, { expiresIn: "3d" })
 };
+
 
 const registerUser = async (req, res) => {
 
@@ -36,10 +38,61 @@ const registerUser = async (req, res) => {
     } catch (error) {
         console.log(error);
         res.status(500).json(error);
-
     }
-
-
 };
 
-module.exports = { registerUser };
+
+const loginUser = async(req, res) => {
+
+    const {email,password} = req.body;
+
+    try{
+
+        let user = await userModel.findOne({ email });
+
+        //Validations
+        if (!user) return res.status(400).json("Invalid email or password!");
+        
+        const isValidPassword = await bcrypt.compare(password,user.password)
+        if(!isValidPassword) return res.status(400).json("Invalid email or password!");
+
+        const token = createToken(user._id);
+
+        res.status(200).json({ _id: user._id, name:user.name, email, token })
+
+    }catch(error){
+        console.log(error);
+        res.status(500).json(error);
+
+    }
+}
+
+
+
+const findUser =async(req,res)=>{
+    const userId = req.params.userId;
+    try{
+        const user = await userModel.findById(userId);
+
+        res.status(200).json(user);
+    }catch(error){
+        console.log(error);
+        res.status(500).json(error);
+
+    }
+};
+
+
+const getUsers =async(req,res)=>{
+   
+    try{
+        const users = await userModel.find();
+
+        res.status(200).json(users);
+    }catch(error){
+        console.log(error);
+        res.status(500).json(error);
+
+    }
+};
+module.exports = { registerUser,loginUser,findUser,getUsers };
