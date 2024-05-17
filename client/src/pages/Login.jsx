f5b22f9c4ce77a4d255bb94c8b4f3c5113333c49
Martbul <martbul01@gmail.com>
2024-05-17 12:06:40 +0300

    add registration, authConetxt, login and logout
    registration, login, logout and authContext functionality are completed(front and backend)
    
    Add conditional rendering of the navigation
    
    All form have front and backend validation

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,5 +1,60 @@
+import { Alert, Button, Form, Row, Col, Stack } from "react-bootstrap";
+import { AuthContext } from "../context/AuthContext";
+import { useContext } from "react";
+
 const Login = () => {
-    return (<>Login</>  );
-}
- 
+  const {
+    loginInfo,
+    loginError,
+    loginUser,
+    updateLoginInfo,
+    isLoginLoading,
+    
+  } = useContext(AuthContext);
+  return (
+    <>
+      <Form onSubmit={loginUser}>
+        <Row
+          style={{
+            height: "100vh",
+            justifyContent: "center",
+            paddingTop: "5%",
+          }}
+        >
+          <Col xs={6}>
+            <Stack gap={3}>
+              <h2>Login</h2>
+
+              <Form.Control
+                type="emali"
+                placeholder="Emali"
+                onChange={(e) =>
+                  updateLoginInfo({ ...loginInfo, email: e.target.value })
+                }
+              />
+              <Form.Control
+                type="password"
+                placeholder="Password"
+                onChange={(e) =>
+                  updateLoginInfo({ ...loginInfo, password: e.target.value })
+                }
+              />
+              <Button variant="primary" type="submit">
+                {isLoginLoading ? "Getting you in...": "Login"}
+              </Button>
+
+                {loginError?.error && (<>
+                    <Alert variant="danger">
+                <p>{loginError?.message}</p>
+              </Alert>
+                </>)}
+             
+            </Stack>
+          </Col>
+        </Row>
+      </Form>
+    </>
+  );
+};
+
 export default Login;
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,5 +1,62 @@
+import { Alert, Button, Form, Row, Col, Stack } from "react-bootstrap";
+import { useContext } from "react";
+import { AuthContext } from "../context/AuthContext";
 const Register = () => {
-    return ( <>Register</> );
-}
- 
+  const { registerInfo, updateRegisterInfo,registerError,isRegisterLoading,registerUser } = useContext(AuthContext);
+
+  return (
+    <>
+      <Form onSubmit={registerUser}>
+        <Row
+          style={{
+            height: "100vh",
+            justifyContent: "center",
+            paddingTop: "5%",
+          }}
+        >
+          <Col xs={6}>
+            <Stack gap={3}>
+              <h2>Register</h2>
+
+              <Form.Control
+                type="text"
+                placeholder="Name"
+                onChange={(e) =>
+                  updateRegisterInfo({ ...registerInfo, name: e.target.value })
+                }
+              />
+              <Form.Control
+                type="emali"
+                placeholder="Emali"
+                onChange={(e) =>
+                  updateRegisterInfo({ ...registerInfo, email: e.target.value })
+                }
+              />
+              <Form.Control
+                type="password"
+                placeholder="Password"
+                onChange={(e) =>
+                  updateRegisterInfo({
+                    ...registerInfo,
+                    password: e.target.value,
+                  })
+                }
+              />
+              <Button variant="primary" type="submit">
+                {isRegisterLoading ? "Creating Your Account": "Register"}
+              </Button>
+                    {
+                        registerError?.error &&  <Alert variant="danger">
+                        <p>{registerError?.message}</p>
+                      </Alert>
+                    }
+             
+            </Stack>
+          </Col>
+        </Row>
+      </Form>
+    </>
+  );
+};
+
 export default Register;
